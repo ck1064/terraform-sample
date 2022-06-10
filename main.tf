92893f1f7c94b5a047dbe400bd8f12ce6e8170f6
chen <chenhe.toronto@gmail.com>
2022-06-10 11:10:38 -0400

    test

--- main.tf
+++ main.tf
@@ -1,15 +1,21 @@
 terraform {
+  required_version = ">= 1.1.7"
+
   required_providers {
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.22.0"
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.32.0"
     }
   }
 }
 
 provider "snowflake" {
-  alias = "sys_admin"
-  role  = "SYSADMIN"
+  alias            = "sys_admin"
+  role             = "SYSADMIN"
+  region           = "AWS_CA_CENTRAL_1"
+  account          = "KJ91939"
+  private_key_path = "../monitor/snow-env/keys/rsa_key.p8"
+  username         = "chen"
 }
 
 resource "snowflake_database" "db" {
@@ -21,6 +27,5 @@ resource "snowflake_warehouse" "warehouse" {
   provider       = snowflake.sys_admin
   name           = "DEV_ETL_WH_CH_2"
   warehouse_size = "small"
-
-  auto_suspend = 10
+  auto_suspend   = 60
 }
